b3e6f1224ef4eccb37500b85e8bc89d4d66c434d
YURIITRUSH <61427834+YURIITRUSH@users.noreply.github.com>
2020-03-16 20:34:03 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,35 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const debug = require('gulp-debug');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const debug = require('gulp-debug');
 var cleancss = require('gulp-cleancss');
 const autoprefixer = require('autoprefixer')
 const sourcemaps = require('gulp-sourcemaps')
 const postcss = require('gulp-postcss')
 const webpack = require('webpack-stream');
 const paths = {
-  'root':'./dist',
-  'html':{
-    'watch':[
-    './source/views/**/*.pug'
+  "root": './dist',
+  "html": {
+    "watch": [
+      "./source/views/**/*.pug"
     ],
-    'source':'./source/views/pages/*.pug',
-    'dest':'./dist'
+    "source": "./source/views/pages/*.pug",
+    "dest": "./dist"
   },
-  'css':{
-    'watch': './source/assets/sass/*.sass',
-    'source':'./source/assets/sass/*.sass',
-    'dest':'./dist/css'
+  "css": {
+    "watch": "./source/assets/sass/*.sass",
+    "source": "./source/assets/sass/*.sass",
+    "dest": "./dist/css"
   },
-  'assets':{
-    'images':{
-      'watch':'./source/assets/images/*',
-      'source':'./source/assets/images/*',
-      'dest':'./dist/images'
-    }
-  },
-  'js':{
-    'watch':'./source/assets/js/main.js',
-    'source':'./source/assets/js/main.js',
-    'dest':'./dist/js'
+  "js": {
+    "watch": "./source/assets/js/main.js",
+    "source": "./source/assets/js/main.js",
+    "dest": "./dist/js"
   }
 }
-let isDev = false
+let isDev = true
 let isProd = !isDev
 var webpackConfig = {
   "output": {
@@ -53,43 +46,39 @@ var webpackConfig = {
   },
   mode: isDev ? 'development' : 'production'
 }
-
+async function buildCss () {
+  return gulp.src(paths.css.source)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleancss({keepBreaks: true}))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(browserSync.stream())
+}
+async function buildHTML () {
+  return gulp.src(paths.html.source)
+    .pipe(pug({ "pretty" : true }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream())
+}
 async function buildJs() {
   return gulp.src(paths.js.source)
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest(paths.js.dest))
     .pipe(browserSync.stream())
 }
-
-async function buildCss() {
-  return gulp.src(paths.css.source)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleancss({keepBreaks: false}))
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.css.dest))
-    .pipe(browserSync.stream())
-}
-
-async function buildHTML() {
-  return gulp.src(paths.html.source)
-    .pipe(pug({'pretty':true}))
-    .pipe(gulp.dest(paths.html.dest))
-    .pipe(browserSync.stream())
-}
-
-async function watch() {
+async function watch () {
   browserSync.init({
-    server:{
-      baseDir:paths.root
+    server: {
+      baseDir: paths.root
     }
   })
-  gulp.watch(paths.js.watch, buildJs)
   gulp.watch(paths.html.watch, buildHTML)
   gulp.watch(paths.css.watch, buildCss)
+  gulp.watch(paths.js.watch, buildJs)
 }
-exports.js = buildJs
-exports.css = buildCss
-exports.templates = buildHTML
-exports.watch = watch
+exports.js = buildJs;
+exports.css = buildCss;
+exports.templates = buildHTML;
+exports.watch = watch;
